6870d8164b57b2fcafcd8057b5bc30af1c301c32
Dustin Kurtz <11kurtzd@gmail.com>
2024-02-06 07:32:01 -0500

    CountryCard Progress

--- src/Components/CountryCard.js
+++ src/Components/CountryCard.js
@@ -1,25 +1,25 @@
 import { useState, useEffect } from 'react';
 
 const CountryCard = () => {
-    const [country, setCountry] = useState({});
+    const [country, setCountry] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        const fetchData = async() => {
-            try{
+        const fetchData = async () => {
+            try {
                 const response = await fetch('https://restcountries.com/v3.1/all');
 
                 if(!response.ok) {
                     throw new Error(`HTTP Error! Status: ${response.status}`);
                 }
                 const result = await response.json();
-                const firstCountry = Object.keys(result)[0];
-                setCountry(result[firstCountry]);
+                const firstCountry = result[0];
+                setCountry(result);
             }
          catch(err) {
             console.error('Error fetching data:', err);
-            setError('Error fethcing data. Please try again.');
+            setError('Error fetching data. Please try again.');
         } finally {
             setLoading(false);
         }
@@ -29,9 +29,21 @@ const CountryCard = () => {
 
 return(
     <>
-    <p>{country.name.common}</p>
+        {loading && <p>Loading...</p>}
+        {error && <p>Error: {error}</p>}
+        {country && (
+            <ul className="text-sm flex-col justify-center w-full">
+                {country.map((item, index) => (
+                    <li key={index}>
+                        <img src={item.flags.png}></img>
+                        <p>{item.name?.common}</p>
+                    </li>
+                ))}
+                
+            </ul>
+        )}
     </>
-)
-}
+    );
+};
 
 export default CountryCard;
